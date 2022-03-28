87ef8f8580849e604e22ab392a83d799f7b28cc0
space-nuko <space_nuko@gmail.com>
2022-03-28 00:49:11 -0700

    mascara optimization

--- Source/obj/her/Tears.hx
+++ Source/obj/her/Tears.hx
@@ -45,10 +45,19 @@ class Tears extends openfl.display.MovieClip {
 	public var lastSmudgeTarget:Point = new Point(-1, -1);
 	public var lastSmudge:Point = new Point(-1, -1);
 	public var currentSmudge:Point;
-	public var smudgeVec:Point;
+	public var smudgeVec:Point = new Point();
 	public var smudgeSteps:UInt = 0;
 	public var smudgeToggle:Bool = false;
 	public var smudgeBrush:Vector<Point> = new Vector<Point>();
+
+    private var __mascaraRect: Rectangle = new Rectangle(0, 0, 50, 20);
+    private var __mascaraMatrix: Matrix = new Matrix();
+    private var __mascaraColor: ColorTransform = new ColorTransform();
+
+    private var __lowerEyelidRect: Rectangle = new Rectangle(0, 0, 50, 0);
+
+    private var __smudgeCol1 = new ARGBObject();
+    private var __smudgeCol2 = new ARGBObject();
 
 	public function new() {
 		var library = swf.exporters.animate.AnimateLibrary.get("sdthx-lib");
@@ -91,6 +100,8 @@ class Tears extends openfl.display.MovieClip {
 			}
 			i++;
 		}
+
+        __mascaraMatrix.translate(-33, -3);
 	}
 
 	public function addLowerEyelidMascaraLayer(param1:MovieClip) {
@@ -119,20 +130,15 @@ class Tears extends openfl.display.MovieClip {
 
 	public function drawLowerEyelidMascara() {
 		this.lowerEyelidData.fillRect(this.lowerEyelidArea, 0);
-		var _loc1_ = new Rectangle(0, 0, 50, 20);
-		var _loc2_ = new Matrix();
-		_loc2_.translate(-33, -3);
-		var _loc3_ = new ColorTransform();
-		this.lowerEyelidData.draw(this.mascaraData, _loc2_, _loc3_, null, _loc1_, true);
+		this.lowerEyelidData.draw(this.mascaraData, __mascaraMatrix, __mascaraColor, null, __mascaraRect, true);
 		this.lowerEyelidClear = false;
 	}
 
 	public function clearLowerEyelid(param1:Float) {
-		var _loc2_:Rectangle = null;
 		if (!this.lowerEyelidClear) {
 			param1 = Math.min(20, param1 * 0.4);
-			_loc2_ = new Rectangle(0, 0, 50, param1);
-			this.lowerEyelidData.fillRect(_loc2_, 0);
+            __lowerEyelidRect.height = param1;
+			this.lowerEyelidData.fillRect(__lowerEyelidRect, 0);
 			if (param1 == 20) {
 				this.lowerEyelidClear = true;
 			}
@@ -190,91 +196,109 @@ class Tears extends openfl.display.MovieClip {
 		this.tearSpotList.splice(param1, 1);
 	}
 
-	public function tick(param1:Float) {
-		var _loc4_:Point = null;
-		var _loc5_ = Math.NaN;
-		var _loc6_:Matrix = null;
-		var _loc7_:ColorTransform = null;
-		var _loc8_ = Math.NaN;
+    private var __point = new Point();
+    private var __matrix = new Matrix();
+    private var __color = new ColorTransform(1, 1, 1, 1);
+
+	public function tick(rot:Float) {
 		this.tearsData.lock();
 		this.mascaraData.lock();
+
 		++this.mascaraFadeTick;
 		if (this.mascaraFadeTick >= this.mascaraFadeRate) {
 			this.mascaraFadeTick = 0;
 			this.oldDataBuffer = this.mascaraData.clone();
 			this.mascaraData.fillRect(this.tearArea, 0);
-			this.mascaraData.merge(this.oldDataBuffer, this.tearArea, new Point(), this.rgbBlendMult, this.rgbBlendMult, this.rgbBlendMult,
+			this.mascaraData.merge(this.oldDataBuffer, this.tearArea, __point, this.rgbBlendMult, this.rgbBlendMult, this.rgbBlendMult,
 				this.alphaBlendMult);
 		}
-		var _loc2_:UInt = this.tearSpotList.length;
-		var _loc3_:UInt = 0;
-		while (_loc3_ < _loc2_) {
-			if (this.tearSpotList[_loc3_].scaleX < 1.5) {
-				this.tearSpotList[_loc3_].scaleX *= 1.01;
-				this.tearSpotList[_loc3_].scaleY *= 1.01;
+
+		var tsj:UInt = this.tearSpotList.length;
+		var tsi:UInt = 0;
+		while (tsi < tsj) {
+			if (this.tearSpotList[tsi].scaleX < 1.5) {
+				this.tearSpotList[tsi].scaleX *= 1.01;
+				this.tearSpotList[tsi].scaleY *= 1.01;
 			} else if (Math.random() > 0.01) {
-				this.releaseTear(_loc3_);
-				_loc3_--;
-				_loc2_--;
+				this.releaseTear(tsi);
+				tsi--;
+				tsj--;
 			}
-			_loc3_++;
+			tsi++;
 		}
-		param1 += this.rotation - 0.15;
-		param1 = -param1 * Math.PI / 180;
+
+		rot += this.rotation - 0.15;
+		rot = -rot * Math.PI / 180;
 		this.oldDataBuffer = this.tearsData.clone();
 		this.tearsData.fillRect(this.tearArea, 0);
-		this.tearsData.merge(this.oldDataBuffer, this.tearArea, new Point(), this.rgbBlendMult, this.rgbBlendMult, this.rgbBlendMult, this.alphaBlendMult);
-		var _loc9_:UInt = this.tearList.length;
-		var _loc10_:UInt = 0;
-		while (_loc10_ < _loc9_) {
-			if (this.tearList[_loc10_].tick()) {
-				if (this.tearList[_loc10_].y < 20) {
-					_loc5_ = param1 - Math.max(0, 20 - this.tearList[_loc10_].y) / 40;
+		this.tearsData.merge(this.oldDataBuffer, this.tearArea, __point, this.rgbBlendMult, this.rgbBlendMult, this.rgbBlendMult, this.alphaBlendMult);
+
+		var j:UInt = this.tearList.length;
+		var i:UInt = 0;
+
+        while (i < j) {
+			if (this.tearList[i].tick()) {
+                var tearAngle = Math.NaN;
+				if (this.tearList[i].y < 20) {
+					tearAngle = rot - Math.max(0, 20 - this.tearList[i].y) / 40;
 				} else {
-					_loc5_ = param1 - (30 - this.tearList[_loc10_].y) / 600;
+					tearAngle = rot - (30 - this.tearList[i].y) / 600;
 				}
-				_loc4_ = new Point(-Math.sin(_loc5_) * this.speed, Math.cos(_loc5_) * this.speed);
-				this.tearList[_loc10_].x += _loc4_.x * this.tearList[_loc10_].speedMult;
-				this.tearList[_loc10_].y += _loc4_.y * this.tearList[_loc10_].speedMult;
-				if (this.tearList[_loc10_].y > this.tearArea.height) {
-					this.tearList.splice(_loc10_, 1);
-					_loc10_--;
-					_loc9_--;
+
+                var tearDx = -Math.sin(tearAngle) * this.speed;
+                var tearDy = Math.cos(tearAngle) * this.speed;
+				this.tearList[i].x += tearDx * this.tearList[i].speedMult;
+				this.tearList[i].y += tearDy * this.tearList[i].speedMult;
+
+				if (this.tearList[i].y > this.tearArea.height) {
+					this.tearList.splice(i, 1);
+					i--;
+					j--;
 				} else {
-					_loc6_ = new Matrix();
-					_loc7_ = new ColorTransform(1, 1, 1, this.tearList[_loc10_].alphaMult);
-					_loc6_.scale(this.tearList[_loc10_].scaling, this.tearList[_loc10_].scaling);
-					_loc6_.rotate(-param1);
-					_loc6_.translate(this.tearList[_loc10_].x, this.tearList[_loc10_].y);
-					this.tearsData.draw(this.tearGraphic, _loc6_, _loc7_);
-					_loc8_ = this.tearList[_loc10_].getAge();
-					if (G.mascara && this.tearList[_loc10_].hasMascara) {
-						if (Math.random() > Math.max(0.7 - G.mascaraAmount * 0.01, _loc8_)) {
-							_loc6_.translate(Math.random() * 2 - 1, Math.random() * 2);
-							_loc7_ = new ColorTransform(1, 1, 1, Math.max(0, 1 - _loc8_) * this.mascaraRGB.a, this.mascaraRGB.r, this.mascaraRGB.g,
-								this.mascaraRGB.b);
-							this.mascaraData.draw(this.mascaraGraphic, _loc6_, _loc7_);
+                    __color.alphaMultiplier = this.tearList[i].alphaMult;
+                    __color.redOffset = 0;
+                    __color.greenOffset = 0;
+                    __color.blueOffset = 0;
+
+                    __matrix.identity();
+					__matrix.scale(this.tearList[i].scaling, this.tearList[i].scaling);
+					__matrix.rotate(-rot);
+					__matrix.translate(this.tearList[i].x, this.tearList[i].y);
+
+					this.tearsData.draw(this.tearGraphic, __matrix, __color);
+					var tearAge = this.tearList[i].getAge();
+
+					if (G.mascara && this.tearList[i].hasMascara) {
+						if (Math.random() > Math.max(0.7 - G.mascaraAmount * 0.01, tearAge)) {
+							__matrix.translate(Math.random() * 2 - 1, Math.random() * 2);
+
+                            __color.alphaMultiplier = Math.max(0, 1 - tearAge) * this.mascaraRGB.a;
+                            __color.redOffset = this.mascaraRGB.r;
+                            __color.greenOffset = this.mascaraRGB.g;
+                            __color.blueOffset = this.mascaraRGB.b;
+
+							this.mascaraData.draw(this.mascaraGraphic, __matrix, __color);
 						}
 					}
 				}
 			} else {
-				this.tearList.splice(_loc10_, 1);
-				_loc10_--;
-				_loc9_--;
+				this.tearList.splice(i, 1);
+				i--;
+				j--;
 			}
-			_loc10_++;
+			i++;
 		}
 		this.tearsData.unlock();
 		this.mascaraData.unlock();
 	}
 
 	public function smudge(param1:Point) {
-		var smudgePoint:Point = null;
 		param1 = this.mascaraLayer.globalToLocal(param1);
 		if (this.lastSmudgeTarget == null) {
 			this.lastSmudgeTarget = param1.clone();
 		}
-		this.smudgeVec = new Point(param1.x - this.lastSmudgeTarget.x, param1.y - this.lastSmudgeTarget.y);
+		this.smudgeVec.x = param1.x - this.lastSmudgeTarget.x;
+        this.smudgeVec.y = param1.y - this.lastSmudgeTarget.y;
 		var smudgeLength = Math.sqrt(this.smudgeVec.x * this.smudgeVec.x + this.smudgeVec.y * this.smudgeVec.y);
 		this.smudgeSteps = Std.int(Math.min(100, Math.ffloor(smudgeLength)));
 		this.smudgeVec.x /= smudgeLength;
@@ -282,9 +306,7 @@ class Tears extends openfl.display.MovieClip {
 		this.currentSmudge = this.lastSmudgeTarget.clone();
 
 		var color1UInt:UInt = 0;
-		var color1 = new ARGBObject();
 		var color2UInt:UInt = 0;
-		var color2 = new ARGBObject();
 
 		this.lastSmudge.x = Math.fround(this.lastSmudgeTarget.x);
 		this.lastSmudge.y = Math.fround(this.lastSmudgeTarget.y);
@@ -300,23 +322,23 @@ class Tears extends openfl.display.MovieClip {
 				if (this.smudgeToggle) {
 					for (smudgePoint in this.smudgeBrush) {
 						color1UInt = this.mascaraData.getPixel32(Std.int(this.lastSmudge.x + smudgePoint.x), Std.int(this.lastSmudge.y + smudgePoint.y));
-						color1.a = color1UInt >>> 24;
-						color1.r = (color1UInt & 16711680) >>> 16;
-						color1.g = (color1UInt & 65280) >>> 8;
-						color1.b = color1UInt & 255;
+						__smudgeCol1.a = color1UInt >>> 24;
+						__smudgeCol1.r = (color1UInt & 16711680) >>> 16;
+						__smudgeCol1.g = (color1UInt & 65280) >>> 8;
+						__smudgeCol1.b = color1UInt & 255;
 
 						color2UInt = this.mascaraData.getPixel32(Std.int(this.currentSmudge.x + smudgePoint.x), Std.int(this.currentSmudge.y + smudgePoint.y));
-						color2.a = color2UInt >>> 24;
-						color2.r = (color2UInt & 16711680) >>> 16;
-						color2.g = (color2UInt & 65280) >>> 8;
-						color2.b = color2UInt & 255;
+						__smudgeCol2.a = color2UInt >>> 24;
+						__smudgeCol2.r = (color2UInt & 16711680) >>> 16;
+						__smudgeCol2.g = (color2UInt & 65280) >>> 8;
+						__smudgeCol2.b = color2UInt & 255;
 
-						var alphaBlend = color1.a / (color1.a + color2.a);
-						color1.a = Std.int(Math.min(250, Math.ffloor((color2.a + color1.a) * 0.5)));
-						color1.r = Math.round(alphaBlend * color1.r + (1 - alphaBlend) * color2.r);
-						color1.g = Math.round(alphaBlend * color1.g + (1 - alphaBlend) * color2.g);
-						color1.b = Math.round(alphaBlend * color1.b + (1 - alphaBlend) * color2.b);
-						color1UInt = color1.a << 24 | color1.r << 16 | color1.g << 8 | color1.b;
+						var alphaBlend = __smudgeCol1.a / (__smudgeCol1.a + __smudgeCol2.a);
+						__smudgeCol1.a = Std.int(Math.min(250, Math.ffloor((__smudgeCol2.a + __smudgeCol1.a) * 0.5)));
+						__smudgeCol1.r = Math.round(alphaBlend * __smudgeCol1.r + (1 - alphaBlend) * __smudgeCol2.r);
+						__smudgeCol1.g = Math.round(alphaBlend * __smudgeCol1.g + (1 - alphaBlend) * __smudgeCol2.g);
+						__smudgeCol1.b = Math.round(alphaBlend * __smudgeCol1.b + (1 - alphaBlend) * __smudgeCol2.b);
+						color1UInt = __smudgeCol1.a << 24 | __smudgeCol1.r << 16 | __smudgeCol1.g << 8 | __smudgeCol1.b;
 
 						this.mascaraData.setPixel32(Std.int(this.currentSmudge.x + smudgePoint.x), Std.int(this.currentSmudge.y + smudgePoint.y), color1UInt);
 					}
